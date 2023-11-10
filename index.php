e6d046374f8b1fab23d24fe48207a4e95bfac2f5
GAbraham_SF <abraham.sanchez@evangeliumnet.onmicrosoft.com>
2023-11-10 14:41:21 -0600

    add library table and sweetalert2

--- index.php
+++ index.php
@@ -12,8 +12,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-    <link href="https://cdn.jsdelivr.net/npm/@sweetalert2/theme-dark@4/dark.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.js"></script>
+
+    <link href="sweetalert2/dark-theme.css" rel="stylesheet">
+    <link href="sweetalert2/sweetalert2.min.css" rel="stylesheet">
+    <script src="sweetalert2/sweetalert2.min.js"></script>
+
     <link href="//cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" rel="stylesheet">
 
     <link href="css/estilos.css" rel="stylesheet">
